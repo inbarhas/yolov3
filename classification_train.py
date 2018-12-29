b514f9b656421ba610e982dbf184e55a832bed7b
Tamir Malka <tamirmal@gmail.com>
2018-12-29 06:58:25 -0800

    WIP classificator

--- classification_train.py
+++ classification_train.py
@@ -5,19 +5,22 @@ from keras.preprocessing.image import ImageDataGenerator
 import PIL
 from keras.preprocessing import image
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
 import os.path
-
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
 
 classes_list = []
 
 network_input_shape = {
-    'vgg16': (224, 224)
+    'vgg16': (224, 224),
+    'mobilenet2': (224, 224)
 }
 
 
@@ -80,7 +83,7 @@ def process_lines(lines, type):
     return data, y
 # End
 
-
+"""
 from keras.applications.inception_v3 import InceptionV3
 def inception_v3_get_model(num_classes):
     base_model = InceptionV3(weights='imagenet', include_top=False)
@@ -101,8 +104,9 @@ def inception_v3_get_model(num_classes):
     model = Model(input=base_model.input, output=predictions)
 
     assert 0  # TODO
+"""
 
-
+"""
 from keras.applications import MobileNet
 def mobilenet_get_model(num_classes):
     base_model = MobileNet(weights='imagenet', include_top=False)  # imports the mobilenet model and discards the last 1000 neuron layer.
@@ -131,6 +135,29 @@ def mobilenet_get_model(num_classes):
     return model
 
 # End
+"""
+
+from keras.applications import MobileNetV2
+def mobilenet2_get_model(num_classes):
+    base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224,224,3))
+    # freeze all layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    base_model.summary()
+
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(1024, activation='relu', name='fc1')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(0.5)(x)
+    predictions = Dense(num_classes, activation='softmax', name='output')(x)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    return model
+
+# End
 
 
 from keras.applications.vgg16 import VGG16
@@ -144,23 +171,26 @@ def vgg16_get_model(num_classes):
 
     vgg.summary()
 
+    print("Using base VGG as feature extractor")
+    vgg_features = Model(inputs=vgg.input, outputs=vgg.output)
+
     x = vgg.output
-    x = Flatten(name='flatten')(x)
-    x = Dense(4096, activation='relu', name='fc1')(x)
-    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.5)(x)
     x = Dense(num_classes, activation='softmax', name='predictions')(x)
 
     my_model = Model(inputs=vgg.input, outputs=x)
     my_model.summary()
 
-    return my_model
+    return my_model, vgg_features
 # End
 
 
 def train_post_classifier(lines, idxs_train, idxs_val, type='vgg16'):
     # prepare data
-    train_data, train_y = process_lines(lines[idxs_train], type)
-    val_data, val_y = process_lines(lines[idxs_val], type)
+    train_data, y = process_lines(lines[idxs_train], type)
+    val_data, vy = process_lines(lines[idxs_val], type)
 
     # prepare model
     num_train = len(idxs_train)
@@ -169,13 +199,14 @@ def train_post_classifier(lines, idxs_train, idxs_val, type='vgg16'):
     top_epochs = 50
 
     print("num classes {}".format(num_classes))
-    train_y = to_categorical(train_y, num_classes)
-    val_y = to_categorical(val_y, num_classes)
-
+    train_y = to_categorical(y, num_classes)
+    val_y = to_categorical(vy, num_classes)
 
     # Get model
     if type == 'vgg16':
-        net = vgg16_get_model(num_classes)
+        net, vgg_features = vgg16_get_model(num_classes)
+    elif type == 'mobilenet2':
+        net = mobilenet2_get_model(num_classes)
     else:
         print("only vgg16 for now")
         assert 0
@@ -184,10 +215,17 @@ def train_post_classifier(lines, idxs_train, idxs_val, type='vgg16'):
     if type == 'vgg16':
         sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9, nesterov=True)
         net.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    elif type == 'mobilenet2':
+        sgd = SGD(lr=0.001, momentum=0.9, nesterov=True)
+        net.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['acc'])
     else:
         print("only vgg16 for now")
         assert 0
 
+    """
+    print("==============================================")
+    print("===== Training CNNs")
+    print("==============================================")
     # define input data generators
     shift = 0.1
     datagen_train = ImageDataGenerator(rotation_range=30, width_shift_range=shift, height_shift_range=shift,
@@ -210,6 +248,103 @@ def train_post_classifier(lines, idxs_train, idxs_val, type='vgg16'):
                       validation_data=datagen_test.flow(val_data, val_y, batch_size=batch_size),
                       validation_steps=steps_per_epoch_val)
     net.save_weights('post_vgg16.h5')
+    print("============================= DONE CNN")
+  """
+
+    print("==========================================")
+    print("==== training SVM")
+    print("==========================================")
+
+    def vgg_extract_features_img_array(img_array, model):
+        x = np.expand_dims(img_array, axis=0)
+        x = preprocess_input(x)
+        features = model.predict(x)
+        return features
+
+    # define input data generators
+    shift = 0.1
+    datagen_train = ImageDataGenerator(rotation_range=30, width_shift_range=shift, height_shift_range=shift,
+                                       horizontal_flip=True, zoom_range=0.2)
+    datagen_train.fit(train_data)
+
+    # For validation, do not rotate. do less augmentation
+    shift = 0.05
+    datagen_test = ImageDataGenerator(width_shift_range=shift, height_shift_range=shift,
+                                      horizontal_flip=True, zoom_range=0.1)
+    datagen_test.fit(val_data)
+
+    samples_train = 50 * len(train_data)
+    svm_x_data = []
+    svm_y_data = []
+    cnt = 0
+    for x_batch, y_batch in datagen_train.flow(train_data, y, batch_size=1):
+        svm_x_data.append(vgg_extract_features_img_array(x_batch[0], vgg_features))
+        svm_y_data.append(y_batch[0])
+        cnt += 1
+        if cnt > samples_train:
+            break
+
+    svm_x_data = np.array(svm_x_data)
+    svm_y_data = np.array(svm_y_data)
+    svm_x_data = np.reshape(svm_x_data, (len(svm_x_data), -1))
+    #    svm_y_data = np.reshape(svm_y_data, (len(svm_y_data), -1))
+
+    param = [
+        {
+            "kernel": ["linear"],
+            "C": [1, 10, 100]
+        },
+#        {
+#            "kernel": ["linear"],
+#            "C": [1, 10, 100, 1000]
+#        },
+        #        {
+        #            "kernel": ["rbf"],
+        #            "C": [1, 10, 100, 1000],
+        #            "gamma": [1e-2, 1e-3, 1e-4, 1e-5]
+        #        }
+    ]
+
+    # request probability estimation
+    svm = SVC(probability=True)
+    # 10-fold cross validation, use 4 thread as each fold and each parameter set can be train in parallel
+    #clf = GridSearchCV(svm, param, cv=10, n_jobs=1, verbose=3)
+    # 4-fold cv
+    clf = GridSearchCV(svm, param, cv=4, n_jobs=1, verbose=3)
+
+    # import ipdb; ipdb.set_trace()
+    clf.fit(svm_x_data, svm_y_data)
+    print("\nBest parameters set:")
+    print(clf.best_params_)
+    clf = clf.best_estimator_
+
+    print("Run on test set :")
+    samples_val = 20 * len(train_data)
+    val_svm_x_data = []
+    val_svm_y_data = []
+    cnt = 0
+    for x_batch, y_batch in datagen_train.flow(val_data, vy, batch_size=1):
+        val_svm_x_data.append(vgg_extract_features_img_array(x_batch[0], vgg_features))
+        val_svm_y_data.append(y_batch[0])
+        cnt += 1
+        if cnt > samples_val:
+            break
+
+    val_svm_x_data = np.array(val_svm_x_data)
+    val_svm_y_data = np.array(val_svm_y_data)
+
+    val_svm_x_data = np.reshape(val_svm_x_data, (len(val_svm_x_data), -1))
+    #    val_svm_y_data = np.reshape(val_svm_y_data, (len(val_svm_y_data), -1))
+
+    y_predict = clf.predict(val_svm_x_data)
+    labels = sorted(list(set(svm_y_data)))
+    print("\nConfusion matrix:")
+    print("Labels: {0}\n".format(",".join(labels)))
+    print(confusion_matrix(svm_y_data, y_predict, labels=labels))
+
+    print("\nClassification report:")
+    print(classification_report(svm_y_data, y_predict))
+
 
 # End
 
@@ -226,7 +361,7 @@ def main():
 
     idxs_train = [i for i in range(val_idx)]
     idxs_val = [i for i in range(val_idx, len(lines))]
-    train_post_classifier(lines, idxs_train, idxs_val, type='vgg16')
+    train_post_classifier(lines, idxs_train, idxs_val, type='mobilenet2')
 
 
 if __name__ == "__main__":
