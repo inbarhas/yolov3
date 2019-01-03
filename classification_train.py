8a8d5e4ede4faee276159b76c9c3acc575747c25
Tamir Malka <tamirmal@gmail.com>
2019-01-03 12:46:46 -0800

    refactor train post classifier

--- classification_train.py
+++ classification_train.py
@@ -2,7 +2,7 @@ import numpy as np
 import PIL
 from keras.preprocessing import image
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten, BatchNormalization
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Flatten, BatchNormalization, GlobalMaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
 from sklearn.svm import SVC
@@ -17,6 +17,7 @@ from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
 import logging
+import os
 
 classes_list = []
 
@@ -82,219 +83,207 @@ def process_lines(lines, net_type='vgg16'):
 
     data = np.array(data)
     y = np.array(y)
+
+    hist, bin_edges = np.histogram(y, bins=[0, 1, 2, 3, 4, 5, 6])
+    print("hist y:\n{}".format(hist))
+
     return data, y
 # End
 
 ########################################
 #### VGG16
 ########################################
-def vgg16_get_model(num_classes):
+def vgg_get(num_classes):
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
     # freeze all layers
     for layer in base_model.layers:
         layer.trainable = False
 
-    print("Base model VGG summary:")
-    base_model.summary()
+    classifier = base_model.output
+    classifier = GlobalAveragePooling2D()(classifier)
+    classifier = Dense(128, activation='relu')(classifier)
+    classifier = Dropout(0.5)(classifier)
+    classifier = Dense(num_classes, activation='softmax', name='predictions')(classifier)
+    model_classifier = Model(inputs=base_model.input, outputs=classifier)
+    print("model classifier summary:\n")
+    model_classifier.summary()
 
-    x = Sequential()
-    x.add(base_model)
-    x.add(GlobalAveragePooling2D(name='svm_feed'))  # TODO This layer to be used as inputs to SVM
-    x.add(Dense(128, activation='relu'))
-    x.add(Dropout(0.5))
-    x.add(Dense(num_classes, activation='softmax', name='predictions'))
-    print("vgg - with my TOP layers")
-    x.summary()
+    max_features = base_model.output
+    max_features = GlobalMaxPooling2D(name='svm_feed_max')(max_features)
+    model_max = Model(inputs=base_model.input, outputs=max_features)
+    print("model max features:\n")
+    model_max.summary()
 
-    # Making 2 models - one with 2 outputs. and 1 with single output of classification which we will train
-    # I train the classifier_model - as it is defined with single input & output
-    # While the double model has 2 outputs - so will need a special loss during training
-    # TODO notice the 'svm_feed' layer - this will be the output for SVM classifier
-    classifier_model = x
-    double_model = Model(inputs=x.input, outputs=[classifier_model.output, classifier_model.get_layer('svm_feed').output])
+    avg_features = base_model.output
+    avg_features = GlobalAveragePooling2D(name='svm_feed_avg')(avg_features)
+    model_avg = Model(inputs=base_model.input, outputs=avg_features)
+    print("model avg features:\n")
+    model_avg.summary()
 
-    return double_model, classifier_model
+    three_heads = Model(inputs=base_model.input, outputs=[model_classifier.output, model_max.output, model_avg.output])
+    print("3 head model summary:\n")
+    three_heads.summary()
+
+    return (model_classifier, model_avg, model_max, three_heads)
 # End
 
 
-def train_vgg(model, dataset):
+def train_vgg_classifier(model, dataset):
+    print("==== Training a VGG based classifier ====")
     train_data, train_y, val_data, val_y = dataset
 
     print("len of train = {}".format(len(train_data)))
     print("len of val = {}".format(len(val_data)))
 
     # define input data generators
-    shift = 0.1
-    datagen_train = ImageDataGenerator(rotation_range=30, width_shift_range=shift, height_shift_range=shift,
+    shift = 0.05
+    datagen_train = ImageDataGenerator(rotation_range=10, width_shift_range=shift, height_shift_range=shift,
+                                       preprocessing_function=preprocess_input,
                                        horizontal_flip=True, zoom_range=0.2)
     datagen_train.fit(train_data)
 
     # For validation, do not rotate. do less augmentation
     shift = 0.05
-    datagen_test = ImageDataGenerator(width_shift_range=shift, height_shift_range=shift,
-                                      horizontal_flip=True, zoom_range=0.1)
+    datagen_test = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                      width_shift_range=shift, height_shift_range=shift,
+                                      horizontal_flip=True, zoom_range=0.2)
     datagen_test.fit(val_data)
 
     epochs = 200
     batch_size = 32
     steps_per_epoch = int(len(train_data) / batch_size)
     steps_per_epoch_val = int(len(val_data) / batch_size)
-
-    print("len data {}, len val {}".format(len(train_data), len(val_data)))
     print("steps per epoch : {}, val : {}".format(steps_per_epoch, steps_per_epoch_val))
-
-    print("Freezing base")
-    for layer in model.layers[0]:
-        print("layer name : {}".format(layer.name))
-        layer.trainable = False
-
-    model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy',
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=SGD(lr=1e-4, momentum=0.9),
                   metrics=['accuracy'])
+
     model.summary()
+    log_dir = 'vgg_logs'
+    checkpoint = ModelCheckpoint(os.path.join(log_dir, 'chkp_vgg_classifier_best.h5'),
+                                 monitor='val_loss', save_weights_only=True, save_best_only=True, period=1)
+
     model.fit_generator(datagen_train.flow(train_data, train_y, batch_size=batch_size),
                         steps_per_epoch=steps_per_epoch, epochs=epochs, initial_epoch=0,
                         validation_data=datagen_test.flow(val_data, val_y, batch_size=batch_size),
-                        validation_steps=steps_per_epoch_val)
+                        validation_steps=steps_per_epoch_val, callbacks=[checkpoint])
 
-    print("unfreeze all model ...")
-    log_dir = 'vgg_logs/'
-    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-                                 monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, cooldown=3)
-    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1, restore_best_weights=True)
-
-    for layer in model.layers:
-        layer.trainable = True
-
-    model.compile(optimizer=Adam(lr=1e-5), loss='categorical_crossentropy',
+    print("Last steps - attemp to lower LR/early stop")
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, cooldown=5)
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=SGD(lr=1e-4, momentum=0.9),
                   metrics=['accuracy'])
-    model.summary()
+
     model.fit_generator(datagen_train.flow(train_data, train_y, batch_size=batch_size),
                         steps_per_epoch=steps_per_epoch, epochs=2 * epochs, initial_epoch=epochs,
                         validation_data=datagen_test.flow(val_data, val_y, batch_size=batch_size),
-                        validation_steps=steps_per_epoch_val,
-                        callbacks=[checkpoint, reduce_lr, early_stopping])
-    model.save_weights(log_dir + 'vgg_full_trained_weights_final.h5')
-
+                        validation_steps=steps_per_epoch_val, callbacks=[checkpoint, early_stopping, reduce_lr])
+    model.save_weights(os.path.join(log_dir, 'vgg_classifier_final.h5'))
+    print("==== end ====")
     return model
-
-
-def predict_vgg(model, lines):
-    train_data, y = process_lines(lines, 'vgg16')
-    # process lines returns image as arrays
-    data_to_predict = preprocess_input(train_data)
-    y_predict = model.predict(data_to_predict)
-    print("vgg full prediction")
-    print("y=\n{}".format(y))
-    print("y_predict=\n{}".format(y_predict))
-    y_cls = y_predict[0]
-    y_for_svm = y_predict[1]
-    print("y_cls=\n{}".format(np.argmax(y_cls, axis=1)))
-    print("y_for_svm_shape=\n{}".format(y_for_svm.shape))
-
-
-##############################################
-### SVM over VGG features
-##############################################
-def vgg_extract_features_img_array(img_array, model):
-    x = np.expand_dims(img_array, axis=0)
-    x = preprocess_input(x)
-    # model with 2 outputs. 2nd one is the features
-    _, features = model.predict(x)
-    return features
 # End
 
 
-def train_svm(model, dataset):
-    train_data, train_y, val_data, val_y = dataset
+def train_svm(dataset, net_type, pool_type):
+    train_data, train_y = dataset
     print("==========================================")
-    print("==== training SVM")
+    print("==== training SVM : {}, {}".format(net_type, pool_type))
     print("==========================================")
     print("len of train x {}, y {}".format(len(train_data), len(train_y)))
-    print("len of test x {}, y {}".format(len(val_data), len(val_y)))
-    # define input data generators
-    shift = 0.1
-    datagen_train = ImageDataGenerator(rotation_range=30, width_shift_range=shift, height_shift_range=shift,
-                                       horizontal_flip=True, zoom_range=0.2)
-    datagen_train.fit(train_data)
-    samples_train = 50 * len(train_data)
-    svm_x_data = []
-    svm_y_data = []
-    cnt = 0
-    for x_batch, y_batch in datagen_train.flow(train_data, train_y, batch_size=1):
-        svm_x_data.append(vgg_extract_features_img_array(x_batch[0], model))
-        svm_y_data.append(y_batch[0])
-        cnt += 1
-        if cnt > samples_train:
-            break
-
-    print("len of x : {}".format(len(svm_x_data)))
-    print("len of y : {}".format(len(svm_y_data)))
-    print("shape of x[0] : {}".format(svm_x_data[0].shape))
-    print("shape of y[0] : {}".format(svm_y_data[0].shape))
-
-    svm_x_data = np.array(svm_x_data)
-    svm_y_data = np.array(svm_y_data)
-    svm_x_data = np.reshape(svm_x_data, (len(svm_x_data), -1))
-
-    print("shape of x : {}".format(svm_x_data.shape))
-    print("shape of y : {}".format(svm_y_data.shape))
 
     param = [
         {
-            "kernel": ["linear"],
-            "C": [1]
+            "kernel": ["linear", "poly"],
+            "C": [1, 10, 100, 1000],
+            "gamma": ['scale', 'auto'],
         },
     ]
 
     # request probability estimation
-    svm = SVC(probability=True)
+    svm = SVC()
     # 10-fold cross validation, use 4 thread as each fold and each parameter set can be train in parallel
-    clf = GridSearchCV(svm, param, cv=4, n_jobs=1, verbose=3)
-    clf.fit(svm_x_data, svm_y_data)
+    clf = GridSearchCV(svm, param, cv=10, n_jobs=4, verbose=3)
+    clf.fit(train_data, train_y)
     print("\nBest parameters set:")
     print(clf.best_params_)
     clf = clf.best_estimator_
 
-    print("Run on test set :")
-    # validation data generator
-    shift = 0.05
-    datagen_test = ImageDataGenerator(width_shift_range=shift, height_shift_range=shift,
-                                      horizontal_flip=True, zoom_range=0.1)
-    datagen_test.fit(val_data)
-
-    samples_val = 20 * len(train_data)
-    val_svm_x_data = []
-    val_svm_y_data = []
-    cnt = 0
-    for x_batch, y_batch in datagen_train.flow(val_data, val_y, batch_size=1):
-        val_svm_x_data.append(vgg_extract_features_img_array(x_batch[0], model))
-        val_svm_y_data.append(y_batch[0])
-        cnt += 1
-        if cnt > samples_val:
-            break
-
-    val_svm_x_data = np.array(val_svm_x_data)
-    val_svm_x_data = np.reshape(val_svm_x_data, (len(val_svm_x_data), -1))
-    val_svm_y_data = np.array(val_svm_y_data)
-
-    y_predict = clf.predict(val_svm_x_data)
-
-    print("y predicted")
-    print(y_predict)
-    print("val svm y data")
-    print(val_svm_y_data)
-    print("\nConfusion matrix:")
-    print(confusion_matrix(val_svm_y_data, y_predict))
-    print("\nClassification report:")
-    print(classification_report(val_svm_y_data, y_predict))
-
-    out_cls_path = 'svm.dump'
+    out_cls_path = 'svm_' + net_type + '_' + pool_type
+    print("Saving SVM classsifier at : {}".format(out_cls_path))
     joblib.dump(clf, out_cls_path)
-    print("done training svm. best model saved to {}".format(out_cls_path))
+
+    return clf
 # End
 
 
+def test_svm(svm, val_dataset):
+    val_data, val_y = val_dataset
+    y_predict = svm.predict(val_data)
+    print("y predicted")
+    print(y_predict)
+    print("gt y")
+    print(val_y)
+    print("\nConfusion matrix:")
+    print(confusion_matrix(val_y, y_predict))
+    print("\nClassification report:")
+    print(classification_report(val_y, y_predict))
+
+
+def extract_vgg_features(model, dataset):
+    print("==== Extracting AVG & MAX VGG Features ====")
+    train_data, train_y = dataset
+
+    # define input data generators
+    shift = 0.05
+    datagen_train = ImageDataGenerator(rotation_range=10, width_shift_range=shift, height_shift_range=shift,
+                                       preprocessing_function=preprocess_input,
+                                       horizontal_flip=True, zoom_range=0.2)
+    datagen_train.fit(train_data)
+    samples_train = 20 * len(train_data)
+    print("will generate total of {} samples with data-aug".format(samples_train))
+
+    svm_max_f_data = []
+    svm_avg_f_data = []
+    svm_y_data = []
+
+    cnt = 0
+    for x_batch, y_batch in datagen_train.flow(train_data, train_y, batch_size=1):
+        _, max_f, avg_f = model.predict(x_batch)
+        svm_max_f_data.append(max_f)
+        svm_avg_f_data.append(avg_f)
+
+        svm_y_data.append(y_batch[0])
+
+        cnt += 1
+        if cnt > samples_train:
+            break
+
+    print("len of x avg : {}".format(len(svm_avg_f_data)))
+    print("len of x max : {}".format(len(svm_max_f_data)))
+    print("len of y : {}".format(len(svm_y_data)))
+
+    svm_y_data = np.array(svm_y_data)
+
+    svm_max_f_data = np.array(svm_max_f_data)
+    svm_max_f_data = np.reshape(svm_max_f_data, (len(svm_max_f_data), -1))
+
+    svm_avg_f_data = np.array(svm_avg_f_data)
+    svm_avg_f_data = np.reshape(svm_avg_f_data, (len(svm_avg_f_data), -1))
+
+    print("shape of x max : {}".format(svm_max_f_data.shape))
+    print("shape of x avg : {}".format(svm_avg_f_data.shape))
+    print("shape of y : {}".format(svm_y_data.shape))
+
+    print("shape of x avg[0] : {}".format(svm_avg_f_data[0].shape))
+    print("shape of x max[0] : {}".format(svm_max_f_data[0].shape))
+    print("shape of y[0] : {}".format(svm_y_data[0].shape))
+
+    return svm_max_f_data, svm_y_data, svm_avg_f_data, svm_y_data
+# END
+
+
+"""
 ###############################################
 #### Mobilenet
 ###############################################
@@ -378,9 +367,10 @@ def train_mobilenet1(model, dataset):
 def prep_mobilenet(img_array):
     processed_image_mobilenet = mobilenet.preprocess_input(img_array.copy())
     return processed_image_mobilenet
+"""
+################################################
+################################################
 
-################################################
-################################################
 
 def train_post_classifier(lines, idxs_train, idxs_val):
     # prepare data
@@ -388,21 +378,33 @@ def train_post_classifier(lines, idxs_train, idxs_val):
     train_data, y = process_lines(lines[idxs_train], net_type='vgg16')
     val_data, vy = process_lines(lines[idxs_val], net_type='vgg16')
     num_classes = len(classes_list)
-    print("num classes {}".format(num_classes))
-
     cat_y = to_categorical(y, num_classes)
     cat_vy = to_categorical(vy, num_classes)
+    print("num classes {}".format(num_classes))
+    print("classes list {}".format(classes_list))
 
+    """
     ### Mobilenet ###
-    #my_mobilenet = mobilenet1_get_model(num_classes)
-    #train_mobilenet1(my_mobilenet, [train_data, cat_y, val_data, cat_vy])
+    my_mobilenet = mobilenet1_get_model(num_classes)
+    train_mobilenet1(my_mobilenet, [train_data, cat_y, val_data, cat_vy])
+    """
 
     ### VGG ###
-    vgg_double, vgg_classifier = vgg16_get_model(num_classes)
-    train_vgg(vgg_classifier, [train_data, cat_y, val_data, cat_vy])
+    model_classifier, model_avg, model_max, vgg_all = vgg_get(num_classes)
+    train_vgg_classifier(model_classifier, [train_data, cat_y, val_data, cat_vy])
 
-    ### SVM over VGG ###
-    train_svm(vgg_double, [train_data, y, val_data, vy])
+    svm_max_f_data, svm_max_f_y, svm_avg_f_data, svm_avg_f_y = extract_vgg_features(vgg_all, [train_data, y])
+    vgg_max_svm = train_svm([svm_max_f_data, svm_max_f_y], 'vgg', 'max')
+    vgg_avg_svm = train_svm([svm_avg_f_data, svm_avg_f_y], 'vgg', 'avg')
+    print("==== test VGG ====")
+    prep_svm_val = preprocess_input(val_data)
+    vgg_preds, features_max, features_avg = vgg_all.predict(prep_svm_val)
+    print("\ty_vgg_classifier:\n".format(np.argmax(vgg_preds, axis=1)))
+    print("testing vgg max svm")
+    test_svm(vgg_max_svm, [features_max, vy])
+    print("testing vgg avg svm")
+    test_svm(vgg_avg_svm, [features_avg, vy])
+    print("==== done testing vgg ====")
 
 
 def predict_class(pil_image, boxes, classifier):
